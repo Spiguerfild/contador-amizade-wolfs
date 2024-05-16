11256920f89466b60e8f80ea7590176a0d11cdbd
Spiguerfild <luique16@outlook.com>
2024-05-16 13:05:30 -0400

    aumentando img

--- src/components/NavbarTop/index.jsx
+++ src/components/NavbarTop/index.jsx
@@ -4,15 +4,26 @@ import img from '../../assets/wolfs-img.png';
 
 export const NavbarTop = () => {
   return (
-    <div className="navbar bg-base-200">
-      <div className="flex flex-1 items-center">
-        <button className="px-1 btn btn-ghost">
-          <img src={img} className="h-10 w-auto" alt="Logo" />
-        </button>
+    <>
+      <div className="navbar bg-base-200">
+        <div className="flex flex-1 items-center">
+          <button
+            className="px-1 btn btn-ghost"
+            onClick={() => document.getElementById('my_modal_1').showModal()}
+          >
+            <img src={img} className="h-10 w-auto" alt="Logo" />
+          </button>
+        </div>
+        <div>
+          <ThemeChanger />
+        </div>
       </div>
-      <div>
-        <ThemeChanger />
-      </div>
-    </div>
+
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box">
+          <img src={img} />
+        </div>
+      </dialog>
+    </>
   );
 };
